33a21131f581966bc9c7dcf8f3f5c74b6c1afa4c
fedorath <kurtax.h1@googlemail.com>
2017-04-24 23:17:48 +0100

    Update hi.py

--- hi.py
+++ hi.py
@@ -61,7 +61,7 @@ while True:#While loop which grabs images until it is told to stop.
 	PIC = IMG.getImage()
         PIC2 = IMG.getImage().toGray()
 	time.sleep(1)
-        d = (PIC1 - PIC2).binarize().invert()
+        d = (PIC1 - PIC2).binarize(50).invert()
 
         matrix = d.getNumpy()
         avg = matrix.mean()
