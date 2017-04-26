5f2b8a8bc86d6762d8f1bb5fab4f30b7db4d69d8
fedorath <kurtax.h1@googlemail.com>
2017-04-26 17:12:45 +0100

    Update simple.py

--- simple.py
+++ simple.py
@@ -66,7 +66,7 @@ while True:#While loop which grabs images until it is told to stop.
 	
 	if Time >= (Time_pause  + start):#starts
 		start = time.time()
-		for root, dirs, files in os.walk(Directory, topdown=False):#checks the folder for images
+		for root, dirs, files in os.walk(Directory, topdown=True):#checks the folder for images
 			for file in files:#finds the image
 				Sortfile = sorted(files)[0]#sorts the images
 				mailer = os.path.join(root, Sortfile)
